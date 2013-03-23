ea47c2389dc62526732e3b91f1fd05d7afd2d82c
kiwong <kiwong2k@gmail.com>
2013-03-22 18:23:23 -0700

    directive scope, boy oh boy

--- iwc/static/app/js/directives.js
+++ iwc/static/app/js/directives.js
@@ -6,83 +6,129 @@ iwc.app.
 	directive('iwcngLayout', function() {
 		return {
 			restrict: 'A',
-			scope: {},
+		   	scope: true,
 			link: function(scope, element, attrs) {
 				$(element).css('height', '100%');
 				$(element).css('position', 'relative');
 			},
 			controller: function($scope, $element) {
-				var lefts = $scope.lefts = [];
-				var rights = $scope.rights = [];
-				var tops = $scope.tops = [];
-				var bottoms = $scope.bottoms = [];
-				var centers = $scope.centers = [];
+				var lefts = [], rights = [], tops = [], bottoms = [], centers = [];
+				var marginLeft = 0, marginRight = 0, marginTop = 0,  marginBottom = 0;
+				var ltr = undefined;
 
 				this.layoutTop = function(pane, element) {
 					console.log("layoutTop", pane, element);
 					tops.push(element);
+					ltr = !!ltr;
 				}
 
 				this.layoutBottom = function(pane, element) {
 					console.log("layoutTop", pane, element);
 					bottoms.push(element);
+					ltr = !!ltr;
 				}
 
 				this.layoutLeft = function(pane, element) {
 					console.log("layoutLeft", pane, element);
 					lefts.push(element);
+					ltr = (ltr == undefined) ? true : ltr;
 				}
 
 				this.layoutRight = function(pane, element) {
 					console.log("layoutRight", pane);
 					rights.push(element);
+					ltr = (ltr == undefined) ? true : ltr;
 				}
 
-				this.layoutCenter = function(pane, element) {
-					// only handle left center right  or top center bottom
+				this.left2Right = function() {
 					if (lefts.length || rights.length) {
-
-						var marginLeft = 0, marginRight = 0;
-
-						// cannot use float left/rght because height issue
-						// use position:absolute instead
 						angular.forEach(lefts, function(left, i) {
-
-							$(left).css('position', 'absolute');
-							$(left).css('left', marginLeft);
-							$(left).css('top', '0');
-							$(left).css('bottom', '0');
-
+							$(left).css({
+								'position': 'absolute',
+								'top': marginTop,
+ 								'bottom': marginBottom,
+								'left': marginLeft
+							});
 							marginLeft += $(left).outerWidth(true);
 						});
 
 						angular.forEach(rights, function(right, i) {
-   							$(right).css('position', 'absolute');
-							$(right).css('right', marginRight)
-							$(right).css('top', '0');
-							$(right).css('bottom', '0');
+							$(right).css({
+								'position': 'absolute',
+								'top': marginTop,
+								'right': marginRight,
+								'bottom': marginBottom
+							});
 							marginRight += $(right).outerWidth(true);
 						});
-
-
-						// make the element
-						//      position: relative
-						//      margin-left: marginLeft
-						//      margin-right: marginRight
-						//      width: auto
-						$(element).css('position', 'absolute');
-						$(element).css('left', marginLeft);
-						$(element).css('right', marginRight);
-						$(element).css('width', 'auto');
-						$(element).css('top', '0');
-						$(element).css('bottom', '0');
-
 					}
+				}
+
+				this.top2Bottom = function() {
+					if (tops.length || bottoms.length) {
+						angular.forEach(tops, function(top, i) {
+							$(top).css({
+								'position': 'absolute',
+								'top': marginTop,
+								'right': marginRight,
+								'left': marginLeft
+							});
+							marginTop += $(top).outerHeight(true);
+						});
+
+						angular.forEach(bottoms, function(bottom, i) {
+							$(bottom).css({
+								'position': 'absolute',
+								'right': marginRight,
+								'bottom': marginBottom,
+								'left': marginLeft
+							});
+							marginBottom += $(bottom).outerHeight(true);
+						});
+					}
+				}
+
+
+				this.layoutCenter = function(pane, element) {
+					if (ltr) {
+						this.left2Right();
+						this.top2Bottom();
+					} else {
+						this.top2Bottom();
+						this.left2Right();
+					}
+
+					$(element).css({
+						'position': 'absolute',
+						'top': marginTop,
+						'right': marginRight,
+						'bottom': marginBottom,
+						'left': marginLeft,
+						'width': 'auto'
+					});
 					console.log("layoutCenter", pane, element);
 
 				}
 
 
+			}
+		};
+	}).
+	directive('iwcngTop', function() {
+		return {
+			require: '^iwcngLayout',
+			restrict: 'A',
+			link: function(scope, element, attrs, layoutCtrl) {
+				layoutCtrl.layoutTop(scope, element);
+			}
+		};
+	}).
+	directive('iwcngBottom', function() {
+		return {
+			require: '^iwcngLayout',
+			restrict: 'A',
+			link: function(scope, element, attrs, layoutCtrl) {
+				layoutCtrl.layoutBottom(scope, element);
 			}
 		};
 	}).
@@ -95,9 +141,19 @@ iwc.app.
 			}
 		};
 	}).
+	directive('iwcngRight', function() {
+		return {
+			require: '^iwcngLayout',
+			restrict: 'A',
+			link: function(scope, element, attrs, layoutCtrl) {
+				layoutCtrl.layoutRight(scope, element);
+			}
+		};
+	}).
 	directive('iwcngCenter', function() {
 		return {
 			require: '^iwcngLayout',
+			scope: true,
 			restrict: 'A',
 			link: function(scope, element, attrs, layoutCtrl) {
 				layoutCtrl.layoutCenter(scope, element);
