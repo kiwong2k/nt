4fa8b9d3bfee570a4fe4496fd99fc40fda7283e2
kiwong2k <kiwong2k@gmail.com>
2013-06-04 15:23:54 -0700

    handle checkboxes

--- iwc/static/app/js/mail/controllers.js
+++ iwc/static/app/js/mail/controllers.js
@@ -16,47 +16,76 @@ function IwcMailCtrl($scope, $filter) {
 	
 
 	$scope.selectedFolder = "inbox";
-
 	$scope.selectedFolderDN = "< " + $scope.selectedFolder;
+	$scope.msgs = [];
+	$scope.msgsChecked = [];
 
 	$scope.newFolder = function() {
 		alert("Compose email not yet implemented");
 	}
 
-	$scope.msgs = [
-	
-		{
-			"uid": 5,
-			"from": "ki.wong@oracle.com",
-			"subject": "Convergence Demo 5",
-			"size": 2793,
-			"date": 1359586362000,
-			"shortText": "this is a demo 5",
-			"html": "<h2>this is a demo</h2>"
+	$scope.rowClicked = function(msg) {
+		console.log("row Clicked");
+		$scope.msg = msg;
 
-		},
-		
-		{
-			"uid": 6,
-			"from": "ki.wong@oracle.com",
-			"subject": "Convergence Demo 6",
-			"size": 2793,
-			"date": 1359586362000,
-			"shortText": "this is a demo 6",
-			"html": "<h2>this is a demo</h2>"
-
-		}
-	];
-	
-	$scope.rowClicked = function(msg) {		
-		console.log("row clicked uid="+msg.uid);
-		//selectMsg(msg);
-		var newMsgs = $filter('filter')($scope.msgs, function(m) {
-			return m !== msg;
+		angular.forEach($scope.msgsChecked, function(msg, i) {
+			msg.isChecked = false;
 		});
+		$scope.msgsChecked = [];
+	}
+	
+	$scope.checkBoxClicked = function(msg, $event) {
+		console.log("checkbox Clicked: " + msg.isChecked);
+		$event.stopPropagation();
+		if (msg.isChecked) {
+			$scope.msgsChecked.push(msg);
+			$scope.msg = null;
+		} else {
+			var newMsgs = $filter('filter')($scope.msgsChecked, function(m) {
+				return m !== msg;
+			});
+			$scope.msgsChecked = newMsgs;
+		}
+	}
+	
+	$scope.deleteMsg = function() {
+		if ($scope.msg) {
+			$scope.msgsChecked.push($scope.msg);
+		}
+
+		angular.forEach($scope.msgsChecked, function(msg1, i) {
+			var newMsgs = $filter('filter')($scope.msgs, function(m) {
+				return m !== msg1;
+			});
+			$scope.msgs = newMsgs;			
+		})
+
+		$scope.msg = null;
+		$scope.msgsChecked = [];
+	}
+	
+
+	$scope._fetchMsgs = function(mbox, start, count) {
+		var msgs = [];
+		var uid = 0;
 		
-		$scope.msgs = newMsgs;
+		for (var i=0; i<count; i++) {
+			uid = start+i;
+			msgs.push(
+				{
+					"uid": uid,
+					"from": "ki.wong@oracle.com",
+					"subject": "Demo msg " + uid,
+					"size": 100000 * Math.random(),
+					"shortText": "This is demo " + uid,
+					"html": "<h2>This is demo " + uid
+				}
+			)
+		}
+		
+		return msgs;
 	}
 
+	$scope.msgs = $scope._fetchMsgs("Inbox", 100, 100);
 
 }
