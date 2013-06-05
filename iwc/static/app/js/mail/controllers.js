68d69b3db5e993496c247d151e8ec0b912e959fb
kiwong2k <kiwong2k@gmail.com>
2013-06-05 14:14:13 -0700

    add notice messages for no selection and mutliple selection

--- iwc/static/app/js/mail/controllers.js
+++ iwc/static/app/js/mail/controllers.js
@@ -10,70 +10,15 @@ function IwcMailCtrl($scope, $filter) {
 	];
 
 	$scope.viewpanels = [
-		{"template": 'js/mail/templates/main/viewer/viewerbar.html', "selected": true},
-		{"template": 'js/mail/templates/main/viewer/message.html', "selected": true}
+		{"template": 'js/mail/templates/main/viewer/viewerbar.html', "selected": false},
+		{"template": 'js/mail/templates/main/viewer/notice.html', "selected": true},
+		{"template": 'js/mail/templates/main/viewer/chkboxnotice.html', "selected": false},
+		{"template": 'js/mail/templates/main/viewer/message.html', "selected": false}
 	];
 	
-
-	$scope.selectedFolder = "inbox";
-	$scope.selectedFolderDN = "< " + $scope.selectedFolder;
-	$scope.msgs = [];
-	$scope.msgsChecked = [];
-	$scope.uid = 100;
-
-	$scope.newFolder = function() {
-		alert("Compose email not yet implemented");
-	}
-
-	$scope.getMail = function() {
-		var msgs = $scope._fetchMsgs("Inbox", --$scope.uid, 1);
-		$scope.msgs = msgs.concat($scope.msgs);
-	}
-
-	$scope.rowClicked = function(msg) {
-		console.log("row Clicked");
-		if ($scope.msg) $scope.msg.isSelected = false;
-		$scope.msg = msg;
-		$scope.msg.isSelected = true;
-
-		angular.forEach($scope.msgsChecked, function(msg, i) {
-			msg.isChecked = false;
-		});
-		$scope.msgsChecked = [];
-	}
-	
-	$scope.checkBoxClicked = function(msg, $event) {
-		console.log("checkbox Clicked: " + msg.isChecked);
-		$event.stopPropagation();
-		if (msg.isChecked) {
-			$scope.msgsChecked.push(msg);
-			if ($scope.msg) $scope.msg.isSelected = false;
-			$scope.msg = null;
-		} else {
-			var newMsgs = $filter('filter')($scope.msgsChecked, function(m) {
-				return m !== msg;
-			});
-			$scope.msgsChecked = newMsgs;
-		}
-	}
-	
-	$scope.deleteMsg = function() {
-		if ($scope.msg) {
-			$scope.msgsChecked.push($scope.msg);
-		}
-
-		angular.forEach($scope.msgsChecked, function(msg1, i) {
-			var newMsgs = $filter('filter')($scope.msgs, function(m) {
-				return m !== msg1;
-			});
-			$scope.msgs = newMsgs;			
-		})
-
-		$scope.msg = null;
-		$scope.msgsChecked = [];
-	}
-	
-
+	//
+	// private functions start here
+	//
 	$scope._fetchMsgs = function(mbox, start, count) {
 		var msgs = [];
 		var uid = 0;
@@ -97,6 +42,94 @@ function IwcMailCtrl($scope, $filter) {
 		return msgs;
 	}
 
+	$scope._showPanels = function(arr, names) {
+		function _showPanel(arr, name) {
+			angular.forEach(arr, function(p) {
+				p.selected = p.selected || (p.template.indexOf(name) != -1);
+			});
+		}
+	
+		angular.forEach(arr, function(p) {
+			p.selected = false;
+		});
+		angular.forEach(names, function(n) {
+			_showPanel(arr, n);
+		});
+	}
+
+
+	//
+	// private functions end here
+	//
+
+	// public functions start here
+	$scope.newFolder = function() {
+		alert("Compose email not yet implemented");
+	}
+
+	$scope.getMail = function() {
+		var msgs = $scope._fetchMsgs("Inbox", --$scope.uid, 1);
+		$scope.msgs = msgs.concat($scope.msgs);
+	}
+
+	$scope.rowClicked = function(msg) {
+		console.log("row Clicked");
+		if ($scope.msg) $scope.msg.isSelected = false;
+		$scope.msg = msg;
+		$scope.msg.isSelected = true;
+
+		angular.forEach($scope.msgsChecked, function(msg, i) {
+			msg.isChecked = false;
+		});
+		$scope.msgsChecked = [];
+
+		$scope._showPanels($scope.viewpanels, ["viewerbar.html", "message.html"]);
+	}
+	
+	$scope.checkBoxClicked = function(msg, $event) {
+		console.log("checkbox Clicked: " + msg.isChecked);
+		$event.stopPropagation();
+		if (msg.isChecked) {
+			$scope.msgsChecked.push(msg);
+			if ($scope.msg) $scope.msg.isSelected = false;
+			$scope.msg = null;
+		} else {
+			var newMsgs = $filter('filter')($scope.msgsChecked, function(m) {
+				return m !== msg;
+			});
+			$scope.msgsChecked = newMsgs;
+		}
+
+		$scope._showPanels($scope.viewpanels, ["viewerbar.html", "chkboxnotice.html"]);
+		
+	}
+	
+	$scope.deleteMsg = function() {
+		if ($scope.msg) {
+			$scope.msgsChecked.push($scope.msg);
+		}
+
+		angular.forEach($scope.msgsChecked, function(msg1, i) {
+			var newMsgs = $filter('filter')($scope.msgs, function(m) {
+				return m !== msg1;
+			});
+			$scope.msgs = newMsgs;			
+		})
+
+		$scope.msg = null;
+		$scope.msgsChecked = [];
+
+		$scope._showPanels($scope.viewpanels, ["notice.html"]);
+	}
+
+	// public functions end here
+
+	// initializations
+
+	$scope.selectedFolder = "inbox";
+	$scope.selectedFolderDN = "< " + $scope.selectedFolder;
+	$scope.uid = 100;
 	$scope.msgs = $scope._fetchMsgs("Inbox", $scope.uid, 300);
+	$scope.msgsChecked = [];
 
 }
