b702b0e1f5145840e8abe199ff2335ce1aa5c5c8
kiwong2k <kiwong2k@gmail.com>
2013-11-22 16:38:46 -0800

    convert banner to use  bootstrap 3.x, previously it was using bootstrap 2.x

--- iwc/static/app/iwc/directives/banner.js
+++ iwc/static/app/iwc/directives/banner.js
@@ -10,41 +10,38 @@ iwc.app.directive('banner', function() {
 		},
 		template:
 			'<div class="iwc-banner">' +
-				'<div class="navbar navbar-inverse">' +
-					'<div class="navbar-inner">' +
-						'<div class="container-fluid">' +
-							'<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">' +
+				'<div class="navbar navbar-inverse navbar-static-top">' +
+					'<div class="container">' +
+						'<div class="navbar-header">' +
+							'<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">' +
+								'<span class="sr-only">Toggle navigation</span>' +
 								'<span class="icon-bar"></span>' +
 								'<span class="icon-bar"></span>' +
 								'<span class="icon-bar"></span>' +
-							'</a>' +
-							'<a class="brand" >Convergence</a>' +
-							'<div class="nav-collapse collapse">' +
-								'<ul class="nav">' +
-									'<li class="nav" ng-repeat="panel in $parent.panels" ng-class="{active:panel.selected}">'+
-										'<a href="" ng-click="select(panel)">{{panel.title}}</a>' +
-									'</li>' +
-								'</ul>' +
-								'<ul class="nav pull-right">' +
-									'<li><a href="#"><i class="icon-fire icon-white"></i></a></li>' +
-									'<li><a href="#"><i class="icon-cog icon-white"></i></a></li>' +
-									'<li>' +
-										'<div class="btn-group">' +
-											'<a class="btn" href="#"><i class="glyphicon glyphicon-user"></i> Jeff Lin</a>' +
-											'<a class="btn dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>' +
-											'<ul class="dropdown-menu">' +
-												'<li><a href="#"><i class="icon-pencil"></i> Edit</a></li>' +
-												'<li><a href="#"><i class="icon-trash"></i> Delete</a></li>' +
-												'<li><a href="#"><i class="icon-ban-circle"></i> Ban</a></li>' +
-												'<li class="divider"></li>' +
-												'<li><a href="#"><i class="i"></i> Sign out </a></li>' +
-											'</ul>' +
-										'</div>' +
-									'</li>' +
-								'</ul>' +
-								/*'<p class="navbar-text pull-right">Logged in as <a href="#">username</a></p>' + */
-							'</div>' + //.nav-collapse
+							'</button>' +
+							'<a class="navbar-brand" href="#">Convergence</a>' +
 						'</div>' +
+						'<div class="navbar-collapse collapse">' +
+							'<ul class="nav navbar-nav">' +
+								'<li class="nav" ng-repeat="panel in $parent.panels" ng-class="{active:panel.selected}">'+
+									'<a href="" ng-click="select(panel)">{{panel.title}}</a>' +
+								'</li>' +
+							'</ul>' +
+							'<ul class="nav navbar-nav pull-right">' +
+								'<li><a href="#"><i class="glyphicon glyphicon-fire icon-white"></i></a></li>' +
+								'<li><a href="#"><i class="glyphicon glyphicon-cog icon-white"></i></a></li>' +
+								'<li class="dropdown">' +
+									'<a class="dropdown-toggle" data-toggle="dropdown" href="#">Jeff Lin <b class="caret"></b></a>' +
+									'<ul class="dropdown-menu">' +
+										'<li><a href="#"><i class="glyphicon glyphicon-pencil"></i> Edit</a></li>' +
+										'<li><a href="#"><i class="glyphicon glyphicon-trash"></i> Delete</a></li>' +
+										'<li><a href="#"><i class="glyphicon glyphicon-ban-circle"></i> Ban</a></li>' +
+										'<li class="divider"></li>' +
+										'<li><a href="#"><i class="i"></i> Sign out </a></li>' +
+									'</ul>' +
+								'</li>' +
+							'</ul>' +
+						'</div>' + //.nav-collapse
 					'</div>' +
 				'</div>' +
 			'</div>',
